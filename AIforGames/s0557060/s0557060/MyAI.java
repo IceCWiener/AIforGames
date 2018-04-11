c43b772adb10b9a6fea433be5c8789df7f9a4211
Konstantin Regenhardt <IceCWiener@users.noreply.github.com>
2018-04-11 15:07:05 +0200

    Code durch Prints von Werten erweitert.
    Nur ein paar Prints um zu sehen wofür die Werte stehen. Track und Checkpoints sind bis jetzt immer 0.0. Änderung des Tracks ändert nichts daran.

--- AIforGames/s0557060/s0557060/MyAI.java
+++ AIforGames/s0557060/s0557060/MyAI.java
@@ -5,28 +5,39 @@ import lenz.htw.ai4g.ai.Info;
 
 public class MyAI extends AI {
 	
+	float x;
+	float y;
+	float ori;
+	float checkP;
+	float track;
+	
 	public MyAI(Info info) {
 		super(info);
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "Konstantim";
 	}
 
 	@Override
 	public DriverAction update(boolean wasResetAfterCollision) {
 		
-		info.getX();
-		info.getY();
+		x = info.getX();	//Startpunkt = (500, 500)
+		y = info.getY();
 		info.getAngularVelocity();
-		info.getOrientation();
+		ori = info.getOrientation();	//-3.14 bis 3.14
 		info.getVelocity();
 		info.getCurrentCheckpoint();
 		info.getTrack();
 		
-		return new DriverAction(10, 10);
+		//Consoleprints der Werte
+		//System.out.println("x: " + x + ", y: " + y);
+		//System.out.println("Orientation: " + ori);
+		//System.out.println("Current Checkpoint: " + checkP);
+		//System.out.println("Track: " + track);
+		
+		return new DriverAction(1, 1);
 	}
 	
 	@Override
