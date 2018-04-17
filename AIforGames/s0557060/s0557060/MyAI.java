e8a5d4470b7303ed9272e8946e07cc0c6cdeb0ee
Konstantin Regenhardt <IceCWiener@users.noreply.github.com>
2018-04-17 15:36:07 +0200

    Neue Ai.jar, Neue Winkelmessmethoden, neue Erkenntnisse

--- AIforGames/s0557060/s0557060/MyAI.java
+++ AIforGames/s0557060/s0557060/MyAI.java
@@ -3,7 +3,11 @@ import lenz.htw.ai4g.ai.AI;
 import lenz.htw.ai4g.ai.DriverAction;
 import lenz.htw.ai4g.ai.Info;
 import lenz.htw.ai4g.track.Track;
+import lenz.htw.ai4g.*;
 import java.lang.Math;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.Polygon;
 
 public class MyAI extends AI {
 	
@@ -14,9 +18,21 @@ public class MyAI extends AI {
 	Track track;
 	float richtung;
 	int i = 0;
+	Polygon[] obstacles = info.getTrack().getObstacles(); //Hindernisse vom Typ Polygon
+	int obsX;
+	int obsY;
+	int autoX;
+	int autoY;
 	
 	public MyAI(Info info) {
 		super(info);
+		//nlistForDevelopment(); //Nur zum testen
+		enlistForTournament(557060, 556736); //Beide Matrikelnummern übergeben für Anmeldung als Gruppe
+		
+		//int ziel1 = obstacles[2].npoints; //Zeigt Punkte des Polygons
+		
+//		obstacles[2].xpoints[3]; //Koordinaten des dritten Punktes
+//		obstacles[2].ypoints[3];
 	}
 
 	@Override
@@ -34,16 +50,19 @@ public class MyAI extends AI {
 		info.getVelocity();
 		info.getCurrentCheckpoint();
 		track = info.getTrack();
+		obsX = obstacles[0].xpoints[0];
+		obsY = obstacles[0].ypoints[0];
+		autoX = (int) info.getX();
+		autoY = (int) info.getY();
 		
 		//Consoleprints der Werte
 		//System.out.println("x: " + x + ", y: " + y);
-		//System.out.println("Orientation: " + ori);
+		System.out.println("Orientation: " + ori);
 		//System.out.println("Current Checkpoint: " + checkP);
 		//System.out.println("Track: " + track);
-		
-		richtung = lenkung(x, y, 900, 100);
-		System.out.println(harmReihe());
-		return new DriverAction(1, harmReihe());
+		richtung = lenkung(x, y, obsX, obsY);
+		//System.out.println(harmReihe());
+		return new DriverAction(1, richtung);
 	}
 	
 	@Override
@@ -51,10 +70,10 @@ public class MyAI extends AI {
 		return "/s0557060/car.png";
 	}
 	
-	@Override
-	public boolean isEnabledForRacing() {
-		return true;
-	}
+//	@Override
+//	public boolean isEnabledForRacing() {
+//		return true;
+//	}
 	
 	private int lenkung(float ax, float ay, float bx, float by) {
 		int richtung = 0;
@@ -77,9 +96,12 @@ public class MyAI extends AI {
 		//System.out.println("deltaX: " + deltaX);
 		//System.out.println("deltaY: " + deltaY);
 		//System.out.println("Winkel zwischen den Punkten: " + angle);
+		float punkWi = urschpWink(obsX, obsY);
+		float autoWi = urschpWink(autoX, autoY);
+		
 		
 
-		if (ori > 0.785 + 0.2) {
+		if (ori > 0.785 + 0.2) { //0.785pi = ca 45°
 			richtung = -1;
 			return richtung;
 		} else if(ori < 0.785 - 0.2){
@@ -103,6 +125,31 @@ public class MyAI extends AI {
 			//richtung = (int) Math.pow(-1, i);
 			//System.out.println(richtung);
 			//return richtung;
+	}
+	
+	public float urschpWink(int x, int y) {
+		//float wink;
+		float piWink;
+		float m;
+		m = y / x;
+		piWink = (float) Math.atan(m);
+		return piWink;		
+	}
+	
+	public void doDebugStuff() {
+		int obsX = obstacles[0].xpoints[0];
+		int obsY = obstacles[0].ypoints[0];
+		
+		GL11.glBegin(GL11.GL_LINES);
+		GL11.glVertex2f(info.getX(), info.getY());
+		GL11.glVertex2d(info.getX() + Math.cos(info.getOrientation()) * 15, info.getY() + Math.sin(info.getOrientation()) * 15);
+		GL11.glEnd();
+		GL11.glBegin(GL11.GL_LINES);
+		
+		GL11.glVertex2f(info.getX(), info.getY());
+		GL11.glVertex2f(obsX, obsY);
+		//GL11.glVertex2f(0, 0);
+		GL11.glEnd();
 	}
 
 }
